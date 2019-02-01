ea1b4f090f62f6f865e31e7a1f9d451ea4165189
Tony de Faria <tonydefaria@iigethr.com>
2019-02-01 21:35:20 +0000

    added default page

--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,6 @@
 Rails.application.routes.draw do
   root to: "home#index"
   resources :home, only: [:index], controller: :home
+  resources :default, only: [:index], controller: :default
   resources :courses, only: [:index, :show], controller: :courses
 end
